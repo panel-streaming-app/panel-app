bcbd11158fc0cf7738d61ae4408f61c6c41cc2b9
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-05-15 16:05:26 +0100

    connected db and added in assets

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,34 +1,25 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
 import {
+  boolean,
   index,
   pgTableCreator,
   serial,
-  timestamp,
+  // timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `streaming-app_${name}`);
 
-export const posts = createTable(
-  "post",
+export const media = createTable(
+  "media",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
+    title: varchar("title", { length: 256 }).notNull(),
+    thumbnail: varchar("thumbnail", { length: 2000 }).notNull(),
+    bookmarked: boolean("bookmarked").notNull(),
+    type: varchar("type", { length: 20 }).notNull(),
   },
   (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+    nameIndex: index("name_idx").on(example.title),
+    typeIndex: index("type_idx").on(example.type),
+  }),
 );
