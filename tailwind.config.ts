a1c33fd824c3c8f6ee6b1a6b5e6ec73c933773a4
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-05-16 09:19:29 +0100

    feat: scaffold topNav and add styling and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        "primary-100": "#161D2F",
+        "secondary-100": "#FC4747",
+        "tertiary-100": "#10141E",
+        "tertiary-50": "#5A698F",
+      },
     },
   },
   plugins: [],
